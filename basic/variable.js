8a98bf82eb2ceca6a595b2357555cfd9275946ce
sartaj-alam <116719984+sartaj-alam@users.noreply.github.com>
2024-01-15 13:20:58 +0000

    first day second iteration is done

---
+++ basic/variable.js
@@ -0,0 +1,13 @@
+const accountId = 1;
+let accountEmail = "account@gmail.com"
+var accountPassword = "12345"
+accountCity = "Hazaribagh"
+
+ 
+// accountId = 2;    not allowes
+accountEmail = "account1@gmail.com"
+accountPassword = "123456"
+accountCity = "RajDhanwar"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
